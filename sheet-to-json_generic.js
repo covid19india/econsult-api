acec8046169914877faa1ae2c0f246810b26ff87
Sourav Dey <userhasnoname0@gmail.com>
2020-04-11 16:33:02 +0530

    Modified tab ids

--- sheet-to-json_generic.js
+++ sheet-to-json_generic.js
@@ -5,6 +5,20 @@ const c = require("./lib/constants");
 (async function main() {
   console.log("Running task on start...");
   await task({
+    sheet: c.SPREAD_SHEET_LINKS,
+    tabs: {
+      links: c.TAB_LINKS
+    },
+    file: c.FILE_LINKS
+  });
+  await task({
+    sheet: c.SPREAD_SHEET_DOCTORS,
+    tabs: {
+      doctors: c.TAB_DOCTOR
+    },
+    file: c.FILE_DOCTORS
+  });
+  await task({
     sheet: c.SPREAD_SHEET_FAQ,
     tabs: {
       faq: c.TAB_FAQ
@@ -17,20 +31,6 @@ const c = require("./lib/constants");
       tnc: c.TAB_TNC
     },
     file: c.FILE_TNC
-  });
-  await task({
-    sheet: c.SPREAD_SHEET_LINKS,
-    tabs: {
-      links: c.TAB_LINKS
-    },
-    file: c.FILE_LINKS
-  });
-  await task({
-    sheet: c.SPREAD_SHEET_DOCTORS,
-    tabs: {
-      faq: c.TAB_DOCTOR
-    },
-    file: c.FILE_DOCTORS
   });
   console.log("End of sheet-to-json_generic");
 })();
